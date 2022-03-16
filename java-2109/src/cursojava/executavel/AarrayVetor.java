6ac88c2feba9d3d8c6cee2ba77e7aa4e5a3e0529
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-03-16 14:36:34 -0300

    array com entrada e leitura de forma dinâmica

--- java-2109/src/cursojava/executavel/AarrayVetor.java
+++ java-2109/src/cursojava/executavel/AarrayVetor.java
@@ -7,22 +7,17 @@ public class AarrayVetor {
 		
 		try {
 		
-		/*Array pode ser de todos os tipos de dados e objetos também*/
-		//para tornar um array, basta colocar um "entre chaves" no final da variavel ou no final da decalaração do tipo da variavel
-		//Array sempre deve ter quantidade de posições definidas
-		
-		
-		double[] notas = new double [5];
-		
-		/*Atribuir valor nas posições do Array*/
-		notas [0] = 9.8;
-		notas [1] = 7.8;
-		notas [2] = 4.9;
-		notas [3] = 6.6;
-		notas [4] = 5.3;
+		String posicoes = JOptionPane.showInputDialog(null, "Quantas posições o array deve ter? ");
+		double [] notas = new double [Integer.parseInt(posicoes)];
 		
 		for (int pos = 0; pos < notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição "+ (pos+1));
+			notas [pos] = Double.parseDouble(valor);
+		}
+				
+		for (int pos = 0; pos < notas.length; pos++) {
 			System.out.println("Nota "+(pos+1) + " é = "+notas[pos]);
+	
 		}
 		
 	}catch (Exception e) {
