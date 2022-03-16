d5e4cf92f1a1fd753d4b859aa9e4619cf1088112
Douglas Oliveira <dougmoroli@gmail.com>
2022-03-15 22:23:58 -0300

    iniciando um Array

---
+++ java-2109/src/cursojava/executavel/AarrayVetor.java
@@ -0,0 +1,21 @@
+package cursojava.executavel;
+
+
+public class AarrayVetor {
+	public static void main(String[] args) {
+		/*Array pode ser de todos os tipos de dados e objetos também*/
+		//para tornar um array, basta colocar um "entre chaves" no final da variavel ou no final da decalaração do tipo da variavel
+		//Array sempre deve ter quantidade de posições definidas
+		
+		
+		double[] notas = new double [4];
+		
+		/*Atribuir valor nas posições do Array*/
+		notas [0] = 9.8;
+		notas [1] = 7.8;
+		notas [2] = 4.9;
+		notas [3] = 6.6;
+		System.out.println(notas);
+	}
+}
+
