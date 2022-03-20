9fbdba929f1c3963835ed53eb525d36a5bbf748a
Douglas Oliveira <dougmoroli@gmail.com>
2022-03-20 12:47:49 -0300

    a13.15 - finalização do módulo 13

--- java-2109/src/cursojava/executavel/SplitArray.java
+++ java-2109/src/cursojava/executavel/SplitArray.java
@@ -1,5 +1,9 @@
 package cursojava.executavel;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class SplitArray {
 	public static void main(String[] args) {
 		String texto = "Douglas, cuso Java, 80, 70, 90, 89";
@@ -11,6 +15,20 @@ public class SplitArray {
 		System.out.println("Nota 2: " + valoresArray[3]);
 		System.out.println("Nota 3: " + valoresArray[4]);
 		System.out.println("Nota 4: " + valoresArray[5]);
+		
+		//converterndo um array em uma lista
+		
+		List<String> list = Arrays.asList(valoresArray);
+		
+		for (String valorString : list) {
+			System.out.println(valorString);
+		}
+		//convertendo uma lista em um array
+		String [] converaoArray = list.toArray(new String [6]);
+		
+		for (int pos = 0; pos < converaoArray.length; pos++) {
+			System.out.println(converaoArray[pos]);
+		}
 	}
 
 }
