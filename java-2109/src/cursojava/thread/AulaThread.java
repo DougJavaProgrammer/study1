c171e9eb92d0b8f6f0c34ae13d3b0d12749feaea
Douglas Oliveira <dougmoroli@gmail.com>
2022-03-21 19:23:38 -0300

    criando Thread Paralela

--- java-2109/src/cursojava/thread/AulaThread.java
+++ java-2109/src/cursojava/thread/AulaThread.java
@@ -1,14 +1,36 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
-				
-		for(int pos = 0; pos < 10; pos++) {
-			Thread.sleep(1000); //cria um tempo para cada execução - no exemplo 1000 milisegundos, ou 1 segundo
-			/*Quero executar esse envio com um tempo de parada ou um tempo determinado*/
-			System.out.println("Executando alguma rotina, por exemplo, envio de e-mail");
-		}
+
+		// Thread processando em paralelo
+		new Thread() {
+			public void run() {// executa o que queremos que fique em paralelo
+				// Código da rotina
+
+				/* código da rotina que será executado em paralelo */
+				for (int pos = 0; pos < 10; pos++) {
+					try {
+						Thread.sleep(1000);// cria um tempo para cada execução - no exemplo 1000 milisegundos, ou 1
+											// segundo
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					System.out.println("Executando alguma rotina, por exemplo, envio de e-mail");
+				}
+				/* Fim do código em paralelo */
+			};
+
+		}.start();// inicia a thread de processamento paralelo
+
+		/* Código do sistema do usuário continua o fluxo de trabalho */
 		System.out.println("Programa finalizado");
+
+		/* Fluxo do sistema */
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
 
 }
