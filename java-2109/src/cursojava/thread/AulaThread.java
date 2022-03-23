e2b0a6aa42dc4780abb7840bf5c5f0cc54628c5c
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-03-23 13:52:22 -0300

    processamento concorrente entre duas thread

--- java-2109/src/cursojava/thread/AulaThread.java
+++ java-2109/src/cursojava/thread/AulaThread.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
 
-		// Thread processando em paralelo
+		// Thread processando em paralelo para envio de email
 		new Thread() {
 			public void run() {// executa o que queremos que fique em paralelo
 				// Código da rotina
@@ -26,6 +26,29 @@ public class AulaThread {
 
 		}.start();// inicia a thread de processamento paralelo
 
+		
+		
+		// Thread 2 processando em paralelo para envio de notal fiscal
+				new Thread() {
+					public void run() {// executa o que queremos que fique em paralelo
+						// Código da rotina
+
+						/* código da rotina que será executado em paralelo */
+						for (int pos = 0; pos < 10; pos++) {
+							try {
+								Thread.sleep(1000);// cria um tempo para cada execução - no exemplo 1000 milisegundos, ou 1
+													// segundo
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+
+							System.out.println("Executando alguma rotina, por exemplo, envio de nota fiscal");
+						}
+						/* Fim do código em paralelo */
+					};
+
+				}.start();// inicia a thread de processamento paralelo
+		
 		/* Código do sistema do usuário continua o fluxo de trabalho */
 		System.out.println("Programa finalizado");
 
