043c72eb4dca306e554d68fb3a775bdf635276a2
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-03-24 14:42:00 -0300

    adicionando botoes JButton

--- java-2109/src/cursojava/thread/TelaTimeThread.java
+++ java-2109/src/cursojava/thread/TelaTimeThread.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,9 +22,12 @@ public class TelaTimeThread extends JDialog{ //necessário extender a classe JDia
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
 	
+	private JButton jButton = new JButton("Start");
+	private JButton jButton2 = new JButton("Stop");
+	
 	public TelaTimeThread() { //executa o que tiver dentro no momento de abertura ou execução
 		setTitle("Minha tela de time com Thread"); //título da tela
-		setSize(new Dimension(340, 340)); //tamanho da tela
+		setSize(new Dimension(240, 240)); //tamanho da tela
 		setLocationRelativeTo(null);//centraliza a tela com base em algum componente como nesse caso tem apenas essa tela, usa-se parâmetro null
 		setResizable(false); //bloqueia a alteração de dimensão da tela
 		/*Primeira papreferredrte Concluída*/
@@ -30,12 +35,16 @@ public class TelaTimeThread extends JDialog{ //necessário extender a classe JDia
 		GridBagConstraints gridBagConstraints = new GridBagConstraints(); //gerenciamento de posições de campos e botões na tela
 		gridBagConstraints.gridx = 0; //posição inicial da primeira linha = 0
 		gridBagConstraints.gridy = 0; //posição inicial da primeira coluna = o
+		gridBagConstraints.gridwidth = 2; //iniciando largura do grid  com 2, siginifica que todos os componentes vao ocupar no maximo dois espaços
+		gridBagConstraints.insets = new Insets(5, 10, 5, 5);//distanciamento entre os componentes
+		gridBagConstraints.anchor = GridBagConstraints.WEST; //componentes alinhados à esquerda
 		
 		descricaoHora.setPreferredSize(new Dimension(200, 25));; //definindo tamanho da label descrição hora
-		jPanel.add(descricaoHora, gridBagConstraints); //adicionado label descricaohora na variavel jPanel e indicado o gerenciador de posições 
+		jPanel.add(descricaoHora, gridBagConstraints); //adicionado label descricaohora na variavel jPanel e indicando o gerenciador de posições 
 		
 		mostraTempo.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy ++; //andou uma posição da coluna Y para baixo para mostrar o segundo componente mostraTempo
+		mostraTempo.setEditable(false);
 		jPanel.add(mostraTempo, gridBagConstraints);
 		
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
@@ -44,8 +53,18 @@ public class TelaTimeThread extends JDialog{ //necessário extender a classe JDia
 		
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy ++;
+		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, gridBagConstraints);
+				
+		gridBagConstraints.gridwidth = 1; //os botoes vao ocupar tamanho 1, para alinhamento
 		
+		jButton.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(jButton, gridBagConstraints);
+		
+		jButton2.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridx ++;
+		jPanel.add(jButton2, gridBagConstraints);
 		
 		add(jPanel,BorderLayout.WEST);
 		
