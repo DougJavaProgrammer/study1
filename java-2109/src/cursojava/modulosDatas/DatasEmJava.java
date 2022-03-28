27f654a318f2a1d749424d6dd6972222fa6174db
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-03-28 10:29:13 -0300

    formatando datas com SimpleDateFormat

--- java-2109/src/cursojava/modulosDatas/DatasEmJava.java
+++ java-2109/src/cursojava/modulosDatas/DatasEmJava.java
@@ -1,10 +1,12 @@
 package cursojava.modulosDatas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		//Classe depreciada, apenas para estudo
 		Date date = new Date();
@@ -14,7 +16,17 @@ public class DatasEmJava {
 		System.out.println("Hora do dia: " + date.getHours());
 		System.out.println("Minuto do dia: " + date.getMinutes());
 		System.out.println("Segundo do dia: " + date.getSeconds());
-		System.out.println("Ano: " + date.getYear());
+		System.out.println("Ano: " + (date.getYear()+ 1900));
+		
+		/*Simple Date Format*/
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy - HH:mm.ss");
+		System.out.println("Data atual em formato padão e String: "+simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyy-MM-dd");
+		System.out.println("Data atual em formato para banco de dados: "+simpleDateFormat.format(date));
+		
+		System.out.println("Obejto Date: "+ simpleDateFormat.parse("1987-10-18"));
 
 	}
 
