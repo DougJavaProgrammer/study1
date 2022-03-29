a2356826b1c4f4a697c32059d4540173025b92ee
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-03-29 16:18:14 -0300

    dias, meses e anos

---
+++ java-2109/src/cursojava/modulosDatas/DatasEmJava5.java
@@ -0,0 +1,20 @@
+package cursojava.modulosDatas;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DatasEmJava5 {
+	public static void main(String[] args) {
+
+		LocalDate localDate = LocalDate.now();
+		System.out.println("Data Atual: " + localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+		System.out.println("Dia da semana: " + localDate.getDayOfWeek().name());
+		System.out.println("Dia do mês: " + localDate.getDayOfMonth());
+		System.out.println("Dia do ano: " + localDate.getDayOfYear());
+		System.out.println("Mês: " + localDate.getMonth());
+		System.out.println("Ano: " + localDate.getYear());
+
+	}
+
+}
