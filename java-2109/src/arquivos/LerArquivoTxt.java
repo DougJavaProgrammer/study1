3b6dcbe7de9d284954228b1f1fdbe0d9f421ce61
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-03-31 16:16:01 -0300

    lendo arquivo txt e criando lista de objetos

--- java-2109/src/arquivos/LerArquivoTxt.java
+++ java-2109/src/arquivos/LerArquivoTxt.java
@@ -3,6 +3,8 @@ package arquivos;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivoTxt {
@@ -11,14 +13,25 @@ public class LerArquivoTxt {
 				new File("C:\\Users\\Douglas\\git\\study1\\java-2109\\src\\arquivos\\arquivo.txt"));
 
 		Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
-
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
 		while (lerArquivo.hasNext()) { // enquanto o lerArquivo tiver dados
 			String linha = lerArquivo.nextLine();
 
 			if (linha != null && !linha.isEmpty()) {
-				System.out.println(linha);
+				String [] dados = linha.split("\\;"); //quebra a linha do arquivo em várias dados separados por ; no array
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNome(dados[0]);
+				pessoa.setEmail(dados[1]);
+				pessoa.setIdade(Integer.parseInt(dados[2]));
+				
+				pessoas.add(pessoa);
+				
 			}
 		}
 		lerArquivo.close();
+		for (Pessoa pessoa : pessoas) { //com a lista em mãos, pode gravar no banco de dados ou fazer qlq coisa....
+			System.out.println(pessoa);
+		}
 	}
 }
