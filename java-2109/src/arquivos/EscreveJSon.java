436c3f6c6ebae3ff2c00d4ebed364ac344b05f3e
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-04-06 10:09:32 -0300

    lendo arquivo em JSON e montando lista em Java

--- java-2109/src/arquivos/EscreveJSon.java
+++ java-2109/src/arquivos/EscreveJSon.java
@@ -1,5 +1,6 @@
 package arquivos;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,6 +8,9 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class EscreveJSon {
 	public static void main(String[] args) throws IOException {
@@ -23,7 +27,7 @@ public class EscreveJSon {
 		usuario2.setCpf("444.456.778-20");
 		usuario2.setLogin("joazinho");
 		usuario2.setSenha("senhajoazinho");
-		
+
 		Usuario usuario3 = new Usuario();
 		usuario3.setNome("Aline");
 		usuario3.setCpf("362.526.648-20");
@@ -43,8 +47,23 @@ public class EscreveJSon {
 		FileWriter fileWriter = new FileWriter(
 				"C:\\Users\\Douglas\\git\\study1\\java-2109\\src\\arquivos\\fileJson.json");
 		fileWriter.write(jsonUser);
-		fileWriter.flush(); //forçar a escrita
+		fileWriter.flush(); // forçar a escrita
 		fileWriter.close();
+
+		// -----------------------------------------------------LENDO O ARQUIVO
+		// JSON----------------------------------------------------------------
+
+		FileReader fileReader = new FileReader(
+				"C:\\Users\\Douglas\\git\\study1\\java-2109\\src\\arquivos\\fileJson.json");
+		JsonArray jsonArray = (JsonArray) JsonParser.parseReader(fileReader);
+		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		
+		for (JsonElement jsonElement : jsonArray) { //cada Json Array é um JsonElement
+			Usuario usuario = new Gson().fromJson(jsonElement, Usuario.class);
+			listaUsuarios.add(usuario);
+		}
+		System.out.println("Leitura do arquivo JSON: " + listaUsuarios);
+
 	}
 
 }
